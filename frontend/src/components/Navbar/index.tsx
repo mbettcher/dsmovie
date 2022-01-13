9f67630f894dc519cf504ce79918d6704fe0ef3e
mbettcher <mtonon.pmg@gmail.com>
2022-01-12 22:02:28 -0300

    MovieCard

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -4,13 +4,13 @@ import './style.css';
 function Navbar() {
     return(
         <header>
-        <nav className='container'>
-          <div className='dsmovie-nav-content'>
+        <nav className="container">
+          <div className="dsmovie-nav-content">
             <h1>DSMovie</h1>
-            <a href="https://github.com/devsuperior">
-              <div className='dsmovie-contact-container'>
+            <a href="https://github.com/devsuperior" target="_blank" rel="noreferrer">
+              <div className="dsmovie-contact-container">
                 <GithubIcon />
-                <p className='dsmovie-contact-link'>/devsuperior</p>
+                <p className="dsmovie-contact-link">/devsuperior</p>
               </div>
             </a>
           </div>
